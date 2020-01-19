0ae408af739bedf83e8e1e16522334caba951fa5
Carlos Diaz <cdiaz86@gmail.com>
2020-01-18 19:37:54 -0500

    Adds zipcode validation.

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-const email = document.querySelector('#email');
+function validateEmail() {
+  const email = document.querySelector('#email');
 
-email.addEventListener('input', () => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity('Email address expected.');
-  } else {
-    email.setCustomValidity('');
-  }
-});
+  email.addEventListener('input', () => {
+    if (email.validity.typeMismatch) {
+      email.setCustomValidity('Email address expected.');
+    } else {
+      email.setCustomValidity('');
+    }
+  });
+}
+
+function validateZip() {
+  const zipcode = document.querySelector('#zipcode');
+
+  zipcode.addEventListener('input', (event) => {
+    if (/^\d{5}(-\d{4})?(?!-)$/.test(event.target.value)) {
+      zipcode.setCustomValidity('');
+    } else {
+      zipcode.setCustomValidity('Please enter a valid zipcode.');
+    }
+  });
+}
+
+function runValidation() {
+  validateEmail();
+  validateZip();
+}
+
+runValidation();
