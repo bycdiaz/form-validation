c5f49d8b163cc29d848c054a5795f5517e77fc0e
Carlos Diaz <cdiaz86@gmail.com>
2020-01-18 20:20:04 -0500

    Adds password confirmation validation.

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
-function validateEmail() {
-  const email = document.querySelector('#email');
+function validation() {
+  const password = document.querySelector('#psw');
 
-  email.addEventListener('input', () => {
-    if (email.validity.typeMismatch) {
-      email.setCustomValidity('Email address expected.');
-    } else {
-      email.setCustomValidity('');
-    }
-  });
-}
+  function validateEmail() {
+    const email = document.querySelector('#email');
 
-function validateZip() {
-  const zipcode = document.querySelector('#zipcode');
+    email.addEventListener('input', () => {
+      if (email.validity.typeMismatch) {
+        email.setCustomValidity('Email address expected.');
+      } else {
+        email.setCustomValidity('');
+      }
+    });
+  }
 
-  zipcode.addEventListener('input', (event) => {
-    if (/^\d{5}(-\d{4})?(?!-)$/.test(event.target.value)) {
-      zipcode.setCustomValidity('');
-    } else {
-      zipcode.setCustomValidity('Please enter a valid zipcode.');
-    }
-  });
-}
+  function validateZip() {
+    const zipcode = document.querySelector('#zipcode');
 
-function runValidation() {
+    zipcode.addEventListener('input', (event) => {
+      if (/^\d{5}(-\d{4})?(?!-)$/.test(event.target.value)) {
+        zipcode.setCustomValidity('');
+      } else {
+        zipcode.setCustomValidity('Please enter a valid zipcode.');
+      }
+    });
+  }
+
+  function validatePassword() {
+    password.addEventListener('input', (event) => {
+      if (/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(event.target.value)) {
+        password.setCustomValidity('');
+      } else {
+        password.setCustomValidity('Please enter a valid password.');
+      }
+    });
+  }
+
+  function confirmPassword() {
+    const confirmedPassword = document.querySelector('#pswconfirm');
+
+    confirmedPassword.addEventListener('input', (event) => {
+      if (event.target.value === password.value) {
+        confirmedPassword.setCustomValidity('');
+      } else {
+        confirmedPassword.setCustomValidity('Passwords do not match.');
+      }
+    });
+  }
+
   validateEmail();
   validateZip();
+  validatePassword();
+  confirmPassword();
 }
 
-runValidation();
+validation();
